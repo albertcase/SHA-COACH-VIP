57eb2a83045ca6a0fef855acc59e798a967d7bac
Demon Zhang <demon.zhang@samesamechina.com>
2016-07-26 16:34:50 +0800

    no message

--- bundle/CoachBundle/ApiController.php
+++ bundle/CoachBundle/ApiController.php
@@ -51,7 +51,7 @@ class ApiController extends Controller {
 	public function cardAction() {
 		//600 pKCDxjrwNnpwUXTcyqzi2R3NZRCQ
 		//800 pKCDxjm3GDEKbK19j_SH7VqFAaag
-		$card = array('600'=>'pKCDxjgJjZLTLr8Q7Ke8Oz_-lsWs', '800'=>'pKCDxjtC1MGogu36UOOjqZ5_YkZM');
+		$card = array('600'=>'pKCDxjrHzatLP0PggxFclXBwLoyY', '800'=>'pKCDxjgsQGKQogXPFPNNDjbCpv6c');
 		//$card = array('600'=>'pGXbRsjjVihQHceLiRMgpFWDkNtU', '800'=>'pGXbRssyzDNSGX7qa6D689Vi_700');
 		//$card = array('600'=>'pKCDxji6wCVuB38LBgBTx3U2yBoQ', '800'=>'pKCDxji6wCVuB38LBgBTx3U2yBoQ');
 		$UserAPI = new \Lib\UserAPI();
