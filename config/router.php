e0de697001d9597fe7cbfb9f47d2d4a380c1e88b
Demon Zhang <demon.zhang@samesamechina.com>
2016-08-22 17:20:26 +0800

    no message

--- config/router.php
+++ config/router.php
@@ -2,5 +2,6 @@
 
 $routers = array();
 $routers['/'] = array('CoachBundle\Site', 'index');
+$routers['/card'] = array('CoachBundle\Site', 'card');
 $routers['/callback'] = array('CoachBundle\Api', 'callback');
 $routers['/api/card'] = array('CoachBundle\Api', 'card');
---
+++ template/site/card.tpl.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>COACH</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" >
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <script type="text/javascript" src="http://coach.samesamechina.com/api/v1/js/912e9ed6-7426-49c0-98f2-903fa4bf0d7a/wechat"></script>
+</head>
+<body>
+<script type="text/javascript">
+var cardListJSON = <?php echo $list;?>
+wx.addCard({
+    cardList: [{
+        cardId: cardListJSON[0].cardId,
+        cardExt: '{"timestamp":"'+cardListJSON[0].cardExt.timestamp+'","signature":"'+cardListJSON[0].cardExt.signature+'","openid":"'+cardListJSON[0].cardExt.openid+'","code":"'+cardListJSON[0].cardExt.code+'"}'
+    }],
+    success: function(res) {
+        var cardList = res.cardList;
+        //alert(JSON.stringfiy(res));
+    },
+    fail: function(res) {
+        //alert(JSON.stringfiy(res));
+    },
+    complete: function(res) {
+        //alert(JSON.stringfiy(res));
+    },
+    cancel: function(res) {
+        //alert(JSON.stringfiy(res));
+    },
+    trigger: function(res) {
+        //alert(JSON.stringfiy(res));
+    }
+});
+
+</script>
+</body>
+</html>
