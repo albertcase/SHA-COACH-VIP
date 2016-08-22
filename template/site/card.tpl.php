da0ad4eb70553a85fbe62d1aaabf4b0d0ff06ce1
Demon Zhang <demon.zhang@samesamechina.com>
2016-08-22 17:29:55 +0800

    no message

--- template/site/card.tpl.php
+++ template/site/card.tpl.php
@@ -11,29 +11,30 @@
 <body>
 <script type="text/javascript">
 var cardListJSON = <?php echo json_encode($list);?>;
-wx.addCard({
-    cardList: [{
-        cardId: cardListJSON[0].cardId,
-        cardExt: '{"timestamp":"'+cardListJSON[0].cardExt.timestamp+'","signature":"'+cardListJSON[0].cardExt.signature+'","openid":"'+cardListJSON[0].cardExt.openid+'","code":"'+cardListJSON[0].cardExt.code+'"}'
-    }],
-    success: function(res) {
-        var cardList = res.cardList;
-        //alert(JSON.stringfiy(res));
-    },
-    fail: function(res) {
-        //alert(JSON.stringfiy(res));
-    },
-    complete: function(res) {
-        //alert(JSON.stringfiy(res));
-    },
-    cancel: function(res) {
-        //alert(JSON.stringfiy(res));
-    },
-    trigger: function(res) {
-        //alert(JSON.stringfiy(res));
-    }
-});
-
+$(document).ready(function() {
+    wx.addCard({
+        cardList: [{
+            cardId: cardListJSON[0].cardId,
+            cardExt: '{"timestamp":"'+cardListJSON[0].cardExt.timestamp+'","signature":"'+cardListJSON[0].cardExt.signature+'","openid":"'+cardListJSON[0].cardExt.openid+'","code":"'+cardListJSON[0].cardExt.code+'"}'
+        }],
+        success: function(res) {
+            var cardList = res.cardList;
+            //alert(JSON.stringfiy(res));
+        },
+        fail: function(res) {
+            //alert(JSON.stringfiy(res));
+        },
+        complete: function(res) {
+            //alert(JSON.stringfiy(res));
+        },
+        cancel: function(res) {
+            //alert(JSON.stringfiy(res));
+        },
+        trigger: function(res) {
+            //alert(JSON.stringfiy(res));
+        }
+    });
+}
 </script>
 </body>
 </html>
